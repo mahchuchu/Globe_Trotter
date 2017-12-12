b0e1bec99afc8d8cf5dcbb3f66d489635d10ad47
Veerparatap Tatla <thisbeveer@gmail.com>
2017-12-11 22:12:30 -0800

    Added Level Two

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float playerSpeed;
+	private Rigidbody rb;
+
+    
+    public bool onGround;
+
+	void Start () {
+		onGround = true;
+		rb = GetComponent<Rigidbody>();
+	}
+
+    void Update(){
+
+		if(onGround) {
+			if (Input.GetButtonDown("Jump"))
+			{
+				rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
+				onGround = false;
+			}
+        }
+
+    }
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Ground"))
+		{
+			onGround = true;
+		}
+	}
+	
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * playerSpeed);
+    }
+}

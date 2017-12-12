1df833b315e745ce89ea3453cbca51701899f64d
Josh Villanueva <Josh@Joshs-MacBook-Pro.local>
2017-12-11 21:55:16 -0800

    Initial commit, Created player, player movement, jumping, and collectable coin prefab.

---
+++ Globe Trotter/Assets/Scripts/CoinController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	public static int Score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(45,0,0) * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			gameObject.SetActive (false);
+			Score++;
+		}
+	}
+}
---
+++ Globe Trotter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	private Vector3 position;
+
+	public LayerMask groundLayer;
+	public float jumpHeight;
+	private float raySize = 1.0f;
+	public bool isGrounded;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		position = transform.position;
+		isGrounded = IsGrounded ();
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+			rb.AddForce (Vector3.up * jumpHeight);
+		}
+	}
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+	private bool IsGrounded(){
+		return Physics.Raycast(position,Vector3.down,raySize,groundLayer);
+	}
+
+}

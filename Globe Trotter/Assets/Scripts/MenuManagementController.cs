204f26fce4edee7583582ee275cd687a06f75436
Veerparatap Tatla <thisbeveer@gmail.com>
2017-12-13 19:22:04 -0800

    Merging

---
+++ Globe Trotter/Assets/Scripts/MenuManagementController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagementController : MonoBehaviour {
+
+	public void LoadGame()
+	{
+		SceneManager.LoadScene("Level 1");
+	}
+
+	public void LoadCredits()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+
+	public void LoadMenuFromCredits()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}

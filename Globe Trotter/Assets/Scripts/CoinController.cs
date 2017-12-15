f300068fc95e66fbf499446e028108bac72a46e1
mahchuchu <matthew.hunter08@gmail.com>
2017-12-14 16:36:12 -0800

    implementing high score and continued score through the levels

--- Globe Trotter/Assets/Scripts/CoinController.cs
+++ Globe Trotter/Assets/Scripts/CoinController.cs
@@ -6,19 +6,36 @@ using UnityEngine.UI;
 public class CoinController : MonoBehaviour {
 
 	public static int Score;
+	public static int hiScore;
 	public Text scoreText;
+	public Text hiScoreText;
 	public AudioSource audiosource;
 
 
 	// Initializes the score UI. 
-	void Start () {
+	void Start ()
+	{
 		Score = 0;
-		SetCountText();
+		hiScore = 0;
+		SetCountText ();
+
+		if (PlayerPrefs.HasKey ("HighScore")) {
+			hiScore = PlayerPrefs.GetInt ("HighScore");
+		}
+
 	}
 	
 	// Rotates the coins.
 	void Update () {
 		transform.Rotate (new Vector3(45,0,0) * Time.deltaTime);
+
+		if (Score > hiScore){
+			hiScore = Score;
+			PlayerPrefs.SetInt("HighScore", hiScore);
+		}
+
+		hiScoreText.text = "High Score: " + hiScore.ToString();
+
 	}
 
 	// This function picks up coins and keeps count displaying it in a text field.
@@ -37,6 +54,7 @@ public class CoinController : MonoBehaviour {
 	void SetCountText()
 	{
 		scoreText.text = "Score: " + Score.ToString();
+
 	}
 
 
